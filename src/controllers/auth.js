6d10d5de071c1f5903d5ba1e9304eb44933dc3d2
uliacop <115625925+uliacop@users.noreply.github.com>
2024-08-19 22:09:10 +0300

    added hw5-auth

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,8 +8,7 @@ import {
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
-
-  res.json({
+  res.status(201).json({
     status: 201,
     message: 'Successfully registered a user!',
     data: user,
