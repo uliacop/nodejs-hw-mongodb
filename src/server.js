4a5c1a263769a0590086fe74ac6f2a99e5a094eb
uliacop <115625925+uliacop@users.noreply.github.com>
2024-08-26 23:43:32 +0300

    added hw7-swagger

--- src/server.js
+++ src/server.js
@@ -6,11 +6,12 @@ import rootRouter from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
-
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 const PORT = Number(env('PORT', '3001'));
 
 export const setupServer = () => {
   const app = express();
+  app.use('/api-docs', swaggerDocs());
 
   app.use(cors());
 
